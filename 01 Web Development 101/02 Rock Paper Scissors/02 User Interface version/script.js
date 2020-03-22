e2941b3ca90b5c30acb69346334fda95ad5ba476
Moudi Kawi <kawimoudi@gmail.com>
2020-03-22 11:01:58 +0000

    Project 01:02:02 - Add interface to Rock, Paper, Scissors game

--- 01 Web Development 101/02 Rock Paper Scissors/02 User Interface version/script.js
+++ 01 Web Development 101/02 Rock Paper Scissors/02 User Interface version/script.js
@@ -1,84 +1,93 @@
+// DOM Selectors
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const score = document.querySelector('h1');
+const state = document.querySelector('h2');
+
+// Scores
 let playerScore = 0;
 let computerScore = 0;
-let ties = 0;
 
-function computerPlay() {
-	let choices = [ 'paper', 'scissors', 'rock' ];
+// Computer Selection
+const getComputerChoice = () => {
+	let choices = [ 'rock', 'paper', 'scissors' ];
 	return choices[Math.floor(Math.random() * choices.length)];
-}
+};
 
-function playerPlay(input) {
-	input = prompt('Rock, paper, or scissors?').toLowerCase();
-
-	while (true) {
-		if (input == 'rock' || input == 'paper' || input == 'scissors') {
-			break;
-		} else {
-			input = prompt('Please enter one of the following: rock, paper, scissors').toLowerCase();
-		}
+// Player Selection
+const getUserChoice = (userInput) => {
+	if (userInput.target.innerText === 'ROCK') {
+		return 'rock';
+	} else if (userInput.target.innerText === 'PAPER') {
+		return 'paper';
+	} else if (userInput.target.innerText === 'SCISSORS') {
+		return 'scissors';
 	}
-	return input;
-}
+};
 
-function playRound(computerSelection, playerSelection) {
-	var computerSelection = computerPlay();
-	var playerSelection = playerPlay();
-
-	if (playerSelection == computerSelection) {
-		ties++;
-		return alert("It's a tie. \nPlayer Score: " + playerScore + ' Computer Score: ' + computerScore);
-	} else if (playerSelection == 'paper') {
-		computerSelection == 'scissors' ? computerScore++ : playerScore++;
-		if (computerSelection == 'scissors') {
-			return alert(
-				'You lose. Scissors beats paper. \nPlayer Score: ' + playerScore + ' Computer Score: ' + computerScore
-			);
-		} else {
-			return alert(
-				'You win. Paper beats rock. \nPlayer Score: ' + playerScore + ' Computer Score: ' + computerScore
-			);
-		}
-	} else if (playerSelection == 'scissors') {
-		computerSelection == 'rock' ? computerScore++ : playerScore++;
-		if (computerSelection == 'rock') {
-			return alert(
-				'You lose. Rock beats scissors. \nPlayer Score: ' + playerScore + ' Computer Score: ' + computerScore
-			);
-		} else {
-			return alert(
-				'You win. Scissors beats paper. \nPlayer Score: ' + playerScore + ' Computer Score: ' + computerScore
-			);
-		}
-	} else if (playerSelection == 'rock') {
-		computerSelection == 'paper' ? computerScore++ : playerScore++;
-		if (computerSelection == 'paper') {
-			return alert(
-				'You lose. Paper beats rock. \nPlayer Score: ' + playerScore + ' Computer Score: ' + computerScore
-			);
-		} else {
-			return alert(
-				'You win. Rock beats scissors. \nPlayer Score: ' + playerScore + ' Computer Score: ' + computerScore
-			);
-		}
-	} else {
-		alert('There was an error with the game.');
+// Round Generator
+const determineWinner = (userChoice, computerChoice) => {
+	if (userChoice === computerChoice) {
+		state.innerText = 'That was a tie';
+	} else if (userChoice === 'rock' && computerChoice === 'paper') {
+		state.innerText = 'Paper beats Rock. The computer won this round!';
+		computerScore++;
+	} else if (userChoice === 'rock' && computerChoice === 'scissors') {
+		state.innerText = 'Rock beats Scissors. You won this round!';
+		playerScore++;
+	} else if (userChoice === 'paper' && computerChoice === 'scissors') {
+		state.innerText = 'Scissors beats Paper. The computer won this round!';
+		computerScore++;
+	} else if (userChoice === 'paper' && computerChoice === 'rock') {
+		state.innerText = 'Paper beats Rock. You won this round!';
+		playerScore++;
+	} else if (userChoice === 'scissors' && computerChoice === 'rock') {
+		state.innerText = 'Rock beats Scissors. The computer won this round!';
+		computerScore++;
+	} else if (userChoice === 'scissors' && computerChoice === 'paper') {
+		state.innerText = 'Scissors beats Paper. You won this round!';
+		playerScore++;
 	}
-}
+};
 
-function game() {
-	for (round = 0; round < 5; round++) {
-		playRound();
+// Event Listeners
+rock.addEventListener('click', (e) => {
+	determineWinner(getUserChoice(e), getComputerChoice());
+	endCheck();
+	score.innerText = playerScore + ':' + computerScore;
+	if (computerScore === 5 || playerScore === 5) {
+		computerScore = 0;
+		playerScore = 0;
 	}
-	if (playerScore > computerScore) {
-		alert('You win. Beautiful!');
-	} else if (playerScore < computerScore) {
-		alert('You lose. Sad!');
-	} else {
-		alert("It's a tie");
-	}
-	playerScore = 0;
-	computerScore = 0;
-	ties = 0;
-}
+});
 
-game();
+paper.addEventListener('click', (e) => {
+	determineWinner(getUserChoice(e), getComputerChoice());
+	endCheck();
+	score.innerText = playerScore + ':' + computerScore;
+	if (computerScore === 5 || playerScore === 5) {
+		computerScore = 0;
+		playerScore = 0;
+	}
+});
+
+scissors.addEventListener('click', (e) => {
+	determineWinner(getUserChoice(e), getComputerChoice());
+	endCheck();
+	score.innerText = playerScore + ':' + computerScore;
+	if (computerScore === 5 || playerScore === 5) {
+		computerScore = 0;
+		playerScore = 0;
+	}
+});
+
+// END GAME CHECK
+const endCheck = () => {
+	if (computerScore === 5 && playerScore < 5) {
+		state.innerText = 'COMPUTER WINS, YOU LOSE!';
+	} else if (playerScore === 5 && computerScore < 5) {
+		state.innerText = 'YOU WIN, COMPUTER LOSES!';
+	}
+};
