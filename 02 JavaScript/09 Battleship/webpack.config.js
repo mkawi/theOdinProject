4f9234e195913b6bc104dd471b21edf9c648a2d6
Moudi Kawi <kawimoudi@gmail.com>
2022-03-14 19:31:49 +0000

    Add tests to 02:09 Battleship project and save milestone

--- 02 JavaScript/09 Battleship/webpack.config.js
+++ 02 JavaScript/09 Battleship/webpack.config.js
@@ -1,8 +1,24 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.js",
 	mode: "development",
+	entry: "./src/index.js",
+	module: {
+		rules: [
+			{
+				test: /\.(scss|css)$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|jpg)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
